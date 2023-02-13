770c4bfca908759c9481304ed66da9549954d0b6
lucas-code42 <lucasdearaujo.brandao@gmail.com>
2023-02-13 20:10:33 -0300

    doubly linked list

---
+++ src/doublyLinkedList/Celula.java
@@ -0,0 +1,45 @@
+package doublyLinkedList;
+
+
+public class Celula {
+    private Object elemento;
+    private Celula proximo;
+    private Celula anterior;
+
+    public Celula(Object elemento) {
+        this(null, elemento);
+    }
+
+    public Celula(Object elemento, Celula proximo) {
+        this.elemento = elemento;
+        this.proximo = proximo;
+    }
+
+    public Celula getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(Celula proximo) {
+        this.proximo = proximo;
+    }
+
+    public Object getElemento() {
+        return elemento;
+    }
+
+    @Override
+    public String toString() {
+        return "Celula{" +
+                "elemento=" + elemento +
+                ", proximo=" + proximo +
+                '}';
+    }
+
+    public Celula getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(Celula anterior) {
+        this.anterior = anterior;
+    }
+}
---
+++ src/doublyLinkedList/DoublyLinkedListTest.java
@@ -0,0 +1,39 @@
+package doublyLinkedList;
+
+public class DoublyLinkedListTest {
+    public static void main(String[] args) {
+        DoublyLinkedList lista = new DoublyLinkedList();
+
+        System.out.println(lista);
+        lista.adicionaNoComeco("teste1");
+        System.out.println(lista);
+        lista.adicionaNoComeco("teste2");
+        System.out.println(lista);
+        lista.adicionaNoComeco("teste3");
+        System.out.println(lista);
+
+        lista.adiciona("teste0");
+        System.out.println(lista);
+
+
+        System.out.println(lista.pegaCelula(1).toString());
+
+
+        lista.adiciona(2, "python");
+        System.out.println(lista);
+
+        Object x = lista.pega(2);
+        System.out.println(x);
+        System.out.println(lista.tamanho());
+
+
+        lista.removeDoComeco(0);
+
+        System.out.println(lista);
+        System.out.println(lista.tamanho());
+
+
+    }
+
+
+}
